4f3c6ba77eccb8051c8152d79084a55ccce662b4
Chima Chukwuebuka <heybooka28@gmail.com>
2020-01-25 03:33:03 +0100

    Added a comment to views.py

--- weathie/views.py
+++ weathie/views.py
@@ -1,8 +1,11 @@
+# This is my views.py
 from django.shortcuts import render
+
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {})
+
 
 def about(request):
     return render(request, 'about.html', {})
